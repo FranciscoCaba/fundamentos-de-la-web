1e79fa9dbb6dab44bcfccfb7d5878cc82dd75d2d
FranciscoCaba <francorv2@fpuna.edu.py>
2022-12-13 15:23:25 -0300

    Segundo desafio de arreglos terminado

--- desafio-arreglos/script.js
+++ desafio-arreglos/script.js
@@ -11,7 +11,22 @@ function siempreHambriento(arr) {
         console.log("Tengo hambre");
 }
 
+console.log("Primer desafio:");
 siempreHambriento([3.14, "comida", "pastel", true, "comida"]);
 // esto debería mostrar "delicioso, "delicioso"
 siempreHambriento([4, 1, 5, 7, 2]);
 // esto debería mostrar "Tengo hambre"
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    // tu código aquí
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log("\nSegundo desafio:");
+console.log(result); // esperamos de vuelta [6, 8, 10, 9]
