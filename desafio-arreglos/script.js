7e7d3d5d824690b3c135b765fa8973e71b38b379
FranciscoCaba <francorv2@fpuna.edu.py>
2022-12-13 15:30:45 -0300

    Cuarto desafio de arreglos terminado

--- desafio-arreglos/script.js
+++ desafio-arreglos/script.js
@@ -1,3 +1,4 @@
+console.log("Primer desafio:");
 function siempreHambriento(arr) {
     var tieneHambre = true;
     console.log(arr);
@@ -10,13 +11,12 @@ function siempreHambriento(arr) {
     if(tieneHambre)
         console.log("Tengo hambre");
 }
-
-console.log("Primer desafio:");
 siempreHambriento([3.14, "comida", "pastel", true, "comida"]);
 // esto debería mostrar "delicioso, "delicioso"
 siempreHambriento([4, 1, 5, 7, 2]);
 // esto debería mostrar "Tengo hambre"
-
+    
+console.log("\nSegundo desafio:");
 function highPass(arr, cutoff) {
     var filteredArr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -27,9 +27,9 @@ function highPass(arr, cutoff) {
     return filteredArr;
 }
 var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-console.log("\nSegundo desafio:");
 console.log(result); // esperamos de vuelta [6, 8, 10, 9]
 
+console.log("\nTercer desafio:");
 function betterThanAverage(arr) {
     var sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -45,5 +45,15 @@ function betterThanAverage(arr) {
     return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
-console.log("\nTercer desafio:");
 console.log(result); // esperamos 4 de vuelta
+
+console.log("\nCuarto desafio:");
+function reverse(arr) {
+    var arrReverse = [];
+    for (let i = arr.length; i >= 0; i--) {
+        arrReverse.push(arr[i]);
+    }
+    return arrReverse;
+}
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // esperamos de vuelta ["e", "d", "c", "b", "a"]
