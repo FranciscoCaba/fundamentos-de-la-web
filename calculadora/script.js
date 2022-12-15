6606966cab23aff9d92a016300ee63a687cafeaa
FranciscoCaba <francorv2@fpuna.edu.py>
2022-12-15 19:35:32 -0300

    Estilos y Logica de operaciones agregadas

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,20 +1,45 @@
 var displayDiv = document.querySelector("#display");
 var value1 = 0;
 var value2 = 0;
+var newNumber = true;
 var operation = "";
 
 function press(value){
-    displayDiv.innerText += value;
+    if(newNumber){
+        displayDiv.innerText = value;
+        newNumber = false;
+    }else{
+        if(displayDiv.innerText.length < 8)
+            displayDiv.innerText += value;
+    }
 }
 
 function clr(){
     displayDiv.innerText = 0;
+    value1 = 0;
+    value2 = 0;
+    newNumber = true;
 }
 
 function setOP(newOP){
+    value1 = displayDiv.innerText;
+    newNumber = true;
     operation = newOP;
 }
 
 function calculate(){
-    
+    value2 = displayDiv.innerText;
+    if(operation == "*")
+        displayDiv.innerText = value1 * value2;
+    else if(operation == "+")
+        displayDiv.innerText = value1 + value2;
+    else if(operation == "/")
+        displayDiv.innerText = value1 / value2;
+    else if(operation == "-")
+        displayDiv.innerText = value1 - value2;
+    else
+        alert("That operation doesnt exist!");
+    num1 = displayDiv.innerText;
+    operation = "";
+    newNumber = true;
 }
