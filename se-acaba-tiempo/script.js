23f982345c30098865713e7f5b18d87b489645ad
FranciscoCaba <francorv2@fpuna.edu.py>
2022-12-15 20:38:10 -0300

    Logica de reloj movil agregada

--- se-acaba-tiempo/script.js
+++ se-acaba-tiempo/script.js
@@ -1,3 +1,7 @@
+const secondsDiv = document.querySelector("#seconds");
+const minutesDiv = document.querySelector("#minutes");
+const hourDiv = document.querySelector("#hour");
+
 function getSecondsSinceStartOfDay() {
     return new Date().getSeconds() + 
     new Date().getMinutes() * 60 + 
@@ -8,4 +12,11 @@ setInterval( function() {
     var time = getSecondsSinceStartOfDay();
     console.log(time);
     // your code here
+    let hora = ((time/3600)%12)*30+180;
+    let minuto = ((time%3600)/60)*6+180;
+    let segundo = ((time%3600)%60)*6+180;
+    
+    hourDiv.style.transform = `rotate(${hora}deg)`;
+    minutesDiv.style.transform = `rotate(${minuto}deg)`;
+    secondsDiv.style.transform = `rotate(${segundo}deg)`;
 }, 1000);
